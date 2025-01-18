e357ef0eedf7a4885c483654433266b6a79bcf32
KhatriSahil2799 <khatrisahil2799@gmail.com>
2025-01-18 23:47:44 +0530

    Added types

---
+++ src/types/index.ts
@@ -0,0 +1,20 @@
+export type FunctionBox = {
+  id: number;
+  equation: string;
+  nextFunction: string;
+};
+
+export type Coordinate = {
+  x: number;
+  y: number;
+};
+
+export type ConnectionNodePositions = {
+  input: Coordinate;
+  output: Coordinate;
+};
+
+export type NodeConnection = {
+  start: Coordinate;
+  end: Coordinate;
+};
